495f76ee62c63ab3099954a9aca93993b071b676
kevinrisqi <kevinrisqi22@gmail.com>
2022-06-10 19:21:52 +0700

    create hash password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,32 @@
 const User = require('../models/User');
 const validation = require('../validation');
-const Joi = require('@hapi/joi');
-
-const schemaUser = Joi.object({
-    name: Joi.string().min(3).required(),
-    email: Joi.string().min(5).required().email(),
-    password: Joi.string().min(6).required(),
-});
+const bcrypt = require('bcryptjs');
 
 
 const insertUser = async (req, res, next) => {
 
+    // Validation
     const { error, value } = validation.registerValidation(req.body);
-    if (error) return res.send(400, error.details[0].message)
+    if (error) return res.send(400, error.details[0].message);
+
+    // Checking if the user is already in the database
+    const emailExist = await User.findOne({email: req.body.email});
+    if (emailExist) return res.send(400, 'Email already exist')
     
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Creater new a user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
 
     try {
         const savedUser = await user.save();
         res.send(201, savedUser);
-        // console.log(savedUser);
         next();
     } catch (error) {
         res.send(400, error);
--- validation.js
+++ validation.js
@@ -2,16 +2,26 @@ const Joi = require('@hapi/joi');
 
 const registerValidation = (data) => {
 
-    const schemaUser = Joi.object({
+    const schema = Joi.object({
         name: Joi.string().min(3).required(),
         email: Joi.string().min(5).required().email(),
         password: Joi.string().min(6).required(),
     });
 
-    return schemaUser.validate(data);
+    return schema.validate(data);
 }
 
+const loginValidation = (data) => {
+
+    const schema = Joi.object({
+        email: Joi.string().min(5).required().email(),
+        password: Joi.string().min(6).required(),
+    });
+
+    return schema.validate(data);
+}
 
 module.exports = {
     registerValidation,
+    loginValidation
 }
